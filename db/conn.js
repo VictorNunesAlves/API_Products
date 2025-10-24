da285c11fe5a3c8c3baecb19fb06b87437695bc3
Victor <victor.alves2@ufu.br>
2025-10-24 17:47:57 -0300

    conectado com database

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('produtosascii', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql' 
-})
+  }
+);
 
-try{
-    sequelize.authenticate();
-    console.log('Conexão com o banco de dados realizada com sucesso!');
-} catch(error){
-    console.log(`Não foi possível conectar ao banco de dados: ${error}`);
-}   
+
 
 module.exports = sequelize;
