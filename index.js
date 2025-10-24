02e8d28ddcf0a6c4f71db513bfb901669804255f
Victor <victor.alves2@ufu.br>
2025-10-24 17:18:24 -0300

    arrumando nomes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const conn = require('./Db/conn');
+const conn = require('./db/conn');
 const cors = require('cors');
 
-const productRoutes = require('./Routes/productRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 
