63dc16e2671d36c4efad6a52ca5652c4d8a20e1b
Victor <victor.alves2@ufu.br>
2025-10-24 17:52:11 -0300

    conectado com database

--- db/conn.js
+++ db/conn.js
@@ -1,16 +1,26 @@
 const { Sequelize } = require('sequelize');
 
+// Estas variaveis deveriam estar ocultas em variaveis de ambiente, e somente serem inseridas no deploy.
+// Mas para fins de estudo e simplicidade do projeto, estao hardcoded aqui.
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  'railway',                          
+  'root',                             
+  'MsggyXkgZkWtbdQOutfCjtGEnIjEUZko', 
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: 'metro.proxy.rlwy.net',     
+    port: 38872,                      
     dialect: 'mysql' 
   }
 );
 
-
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados realizada com sucesso!');
+  } catch (error) {
+    console.log(`Não foi possível conectar ao banco de dados: ${error}`);
+  }
+})();
 
 module.exports = sequelize;
